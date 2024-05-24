7724873482a5c70bc49afaf6c25fbdefc63d3834
Jonathan Fuentes <john.fuen95@gmail.com>
2024-05-23 23:30:37 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings/>
+      <ViewAllJobs />
+    </div>
+  )
+}
+
+export default App
